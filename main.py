6687ac89cd8ee93b4d46165ed99e2d3f6259087c
Ankith <46915066+ankith26@users.noreply.github.com>
2019-11-14 15:27:40 +0530

    Styled the code

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ pygame.init()
 clock = pygame.time.Clock()
 win = pygame.display.set_mode((460, 460))
 pygame.display.set_caption('Chess')
-running = True
 
 wmove = True
 x = y = -100
 sel = [0, 0]
 prevsel = [0, 0]
-wcheckmate = bcheckmate = stalemate = False
+wCheckmate = bCheckmate = stalemate = False
+
 def drawBoard(win):
     colour = (255, 0, 255)
     pygame.draw.rect(win, colour, (0, 0, 460, 30))
@@ -20,119 +20,150 @@ def drawBoard(win):
     pygame.draw.rect(win, colour, (0, 430, 460, 30))
     for y in range(8):
         for x in range(8):
-            if (x + y) % 2 == 0: colour = (255, 255, 255)
-            else: colour = (181,101,29)
-            pygame.draw.rect(win, colour, (30+(x*50), 30+(y*50), 50, 50))
+            if (x + y) % 2 == 0:
+                colour = (255, 255, 255)
+            else:
+                colour = (181, 101, 29)
+            pygame.draw.rect(
+                win, colour, (30 + (x * 50), 30 + (y * 50), 50, 50))
 
-wpawn = pygame.image.load("images/whitePawn.png")
-wrook = pygame.image.load("images/whiteRook.png")
-wknight = pygame.image.load("images/whiteKnight.png")
-wbishop = pygame.image.load("images/whiteBishop.png")
-wqueen = pygame.image.load("images/whiteQueen.png")
-wking = pygame.image.load("images/whiteKing.png")
-bpawn = pygame.image.load("images/blackPawn.png")
-brook = pygame.image.load("images/blackRook.png")
-bknight = pygame.image.load("images/blackKnight.png")
-bbishop = pygame.image.load("images/blackBishop.png")
-bqueen = pygame.image.load("images/blackQueen.png")
-bking = pygame.image.load("images/blackKing.png")
+WPAWN = pygame.image.load("images/whitePawn.png")
+WROOK = pygame.image.load("images/whiteRook.png")
+WKNIGHT = pygame.image.load("images/whiteKnight.png")
+WBISHOP = pygame.image.load("images/whiteBishop.png")
+WQUEEN = pygame.image.load("images/whiteQueen.png")
+WKING = pygame.image.load("images/whiteKing.png")
+BPAWN = pygame.image.load("images/blackPawn.png")
+BROOK = pygame.image.load("images/blackRook.png")
+BKNIGHT = pygame.image.load("images/blackKnight.png")
+BBISHOP = pygame.image.load("images/blackBishop.png")
+BQUEEN = pygame.image.load("images/blackQueen.png")
+BKING = pygame.image.load("images/blackKing.png")
+
 def drawPieces(win):
-    a = 0
+    count = 0
     for x in wBoard:
-        if x != None:
-            if a in range(8): win.blit(wpawn, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a in range(8,10): win.blit(wrook, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a in range(10,12): win.blit(wknight, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a in range(12,14): win.blit(wbishop, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a == 14: win.blit(wqueen, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a == 15: win.blit(wking, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
+        if x is not None:
+            if count in range(8):
+                win.blit(WPAWN, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count in range(8, 10):
+                win.blit(WROOK, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count in range(10, 12):
+                win.blit(WKNIGHT, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count in range(12, 14):
+                win.blit(WBISHOP, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count == 14:
+                win.blit(WQUEEN, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count == 15:
+                win.blit(WKING, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
             else:
                 if x[2] == 'knight':
-                    win.blit(wknight, (x[0]*50 - 20, x[1]*50 - 20))
+                    win.blit(WKNIGHT, (x[0] * 50 - 20, x[1] * 50 - 20))
                 if x[2] == 'bishop':
-                    win.blit(wbishop, (x[0]*50 - 20, x[1]*50 - 20))
+                    win.blit(WBISHOP, (x[0] * 50 - 20, x[1] * 50 - 20))
                 if x[2] == 'rook':
-                    win.blit(wrook, (x[0]*50 - 20, x[1]*50 - 20))
+                    win.blit(WROOK, (x[0] * 50 - 20, x[1] * 50 - 20))
                 if x[2] == 'queen':
-                    win.blit(wqueen, (x[0]*50 - 20, x[1]*50 - 20))
-        a += 1
-    a = 0
+                    win.blit(WQUEEN, (x[0] * 50 - 20, x[1] * 50 - 20))
+        count += 1
+    count = 0
     for x in bBoard:
-        if x != None:
-            if a in range(8): win.blit(bpawn, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a in range(8,10): win.blit(brook, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a in range(10,12): win.blit(bknight, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a in range(12,14): win.blit(bbishop, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a == 14: win.blit(bqueen, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
-            elif a == 15: win.blit(bking, ((x[0]-1)*50 + 30, (x[1]-1)*50 + 30))
+        if x is not None:
+            if count in range(8):
+                win.blit(BPAWN, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count in range(8, 10):
+                win.blit(BROOK, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count in range(10, 12):
+                win.blit(BKNIGHT, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count in range(12, 14):
+                win.blit(BBISHOP, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count == 14:
+                win.blit(BQUEEN, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
+            elif count == 15:
+                win.blit(BKING, ((x[0] - 1) * 50 + 30, (x[1] - 1) * 50 + 30))
             else:
                 if x[2] == 'knight':
-                    win.blit(bknight, (x[0]*50 - 20, x[1]*50 - 20))
+                    win.blit(BKNIGHT, (x[0] * 50 - 20, x[1] * 50 - 20))
                 if x[2] == 'bishop':
-                    win.blit(bbishop, (x[0]*50 - 20, x[1]*50 - 20))
+                    win.blit(BBISHOP, (x[0] * 50 - 20, x[1] * 50 - 20))
                 if x[2] == 'rook':
-                    win.blit(brook, (x[0]*50 - 20, x[1]*50 - 20))
+                    win.blit(BROOK, (x[0] * 50 - 20, x[1] * 50 - 20))
                 if x[2] == 'queen':
-                    win.blit(bqueen, (x[0]*50 - 20, x[1]*50 - 20))
-        a += 1
-while running:
+                    win.blit(BQUEEN, (x[0] * 50 - 20, x[1] * 50 - 20))
+        count += 1
+
+while True:
     clock.tick(24)
     drawBoard(win)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: running = False
+        if event.type == pygame.QUIT:
+            break
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            x,y = pygame.mouse.get_pos()
-            if 30<x<430 and 30<y<430:
+            x, y = pygame.mouse.get_pos()
+            if 30 < x < 430 and 30 < y < 430:
                 x = ((x - 30) // 50) + 1
                 y = ((y - 30) // 50) + 1
                 prevsel = sel
                 sel = [x, y]
-            else: sel = [0, 0]
-            
+            else:
+                sel = [0, 0]
+
     if wmove and sel != [0, 0]:
         if isChecked("w") and isCheckmate("w"):
-            wcheckmate = True
-            running = False
+            wCheckmate = True
+            break
         else:
             stalemate = True
             for i in allMoves("w"):
                 if i[0] == 15:
                     if not isChecked("w", i[1]):
                         stalemate = False
-                else: stalemate = False
-        for i in range(len(wBoard)):          
-            if wBoard[i] != None and prevsel == wBoard[i][:2]:
+                else:
+                    stalemate = False
+        for i in range(len(wBoard)):
+            if wBoard[i] is not None and prevsel == wBoard[i][:2]:
                 if isOccupied(sel[0], sel[1]) != "w":
                     ptype = getPiece("w", prevsel)
                     if sel in availableMoves("w", ptype, prevsel):
-                        if move("w", prevsel, sel): wmove = False
-                        else: wmove = True
+                        if move("w", prevsel, sel):
+                            wmove = False
+                        else:
+                            wmove = True
                         doRoutine()
     elif sel != [0, 0]:
         if isChecked("b") and isCheckmate("b"):
-            bcheckmate = True
-            running = False
+            bCheckmate = True
+            break
         else:
             stalemate = True
             for i in allMoves("b"):
                 if i[0] == 15:
                     if not isChecked("b", i[1]):
                         stalemate = False
-                else: stalemate = False
+                else:
+                    stalemate = False
         for i in range(len(bBoard)):
-            if bBoard[i] != None and prevsel == bBoard[i][:2]:
+            if bBoard[i] is not None and prevsel == bBoard[i][:2]:
                 if isOccupied(sel[0], sel[1]) != "b":
                     ptype = getPiece("b", prevsel)
                     if sel in availableMoves("b", ptype, prevsel):
-                        if move("b", prevsel, sel): wmove = True
-                        else: wmove = False
+                        if move("b", prevsel, sel):
+                            wmove = True
+                        else:
+                            wmove = False
                         doRoutine()
     if stalemate:
-        running = False
-    pygame.draw.rect(win, (255,255,0), (x*50-20, y*50-20, 50, 50))
+        break
+    if wmove and isOccupied(sel[0], sel[1]) == "w":
+        pygame.draw.rect(win, (255, 255, 0), (x * 50 - 20, y * 50 - 20, 50, 50))
+    if not wmove and isOccupied(sel[0], sel[1]) == "b":
+        pygame.draw.rect(win, (255, 255, 0), (x * 50 - 20, y * 50 - 20, 50, 50))
     drawPieces(win)
     pygame.display.update()
-if wcheckmate: print("CHECKMATE! White lost")
-elif bcheckmate: print("CHECKMATE! Black lost")
-elif stalemate: print("DRAW by STALEMATE")
+if wCheckmate:
+    print("CHECKMATE! White lost")
+elif bCheckmate:
+    print("CHECKMATE! Black lost")
+elif stalemate:
+    print("DRAW by STALEMATE")
 pygame.quit()
